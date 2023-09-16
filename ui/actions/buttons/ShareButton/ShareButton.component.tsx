b3af3a2b31b63ea5ab5e8534197983dc1a4e1a0f
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-09-16 12:54:26 +0200

    fixed build

--- ui/actions/buttons/ShareButton/ShareButton.component.tsx
+++ ui/actions/buttons/ShareButton/ShareButton.component.tsx
@@ -7,28 +7,35 @@ import {
 } from "./ShareButton.style";
 
 export const ShareButton: FC = () => {
-  const currentUrl = window.location.href;
+  const currentUrl = typeof window !== "undefined" ? window.location.href : "";
+
   const shareOnFacebook = () => {
-    window.open(
-      `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`,
-      "_blank"
-    );
+    if (typeof window !== "undefined") {
+      window.open(
+        `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`,
+        "_blank"
+      );
+    }
   };
 
   const shareOnTwitter = () => {
-    const text = encodeURIComponent("Check out this awesome article!");
-    window.open(
-      `https://twitter.com/intent/tweet?url=${currentUrl}&text=${text}`,
-      "_blank"
-    );
+    if (typeof window !== "undefined") {
+      const text = encodeURIComponent("Check out this awesome article!");
+      window.open(
+        `https://twitter.com/intent/tweet?url=${currentUrl}&text=${text}`,
+        "_blank"
+      );
+    }
   };
 
   const shareOnLinkedIn = () => {
-    const title = encodeURIComponent("My Website");
-    window.open(
-      `https://www.linkedin.com/shareArticle?url=${currentUrl}&title=${title}`,
-      "_blank"
-    );
+    if (typeof window !== "undefined") {
+      const title = encodeURIComponent("My Website");
+      window.open(
+        `https://www.linkedin.com/shareArticle?url=${currentUrl}&title=${title}`,
+        "_blank"
+      );
+    }
   };
 
   return (
