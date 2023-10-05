feeb35ea7e33511f5205f43bdab0e5751cc43b79
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-10-05 08:51:34 +0200

    working on bookmarks button

--- components/DisplayBookmark/DisplayBookmark.style.tsx
+++ components/DisplayBookmark/DisplayBookmark.style.tsx
@@ -1,32 +1,63 @@
 import { theme } from "@/styles";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export const Container = styled.div``;
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+    z-index: 0;
+    transform: translateY(-100%);
+  }
+  30% {
+    z-index: 0;
+    opacity: 0;
+    
+  }
+  100% {
+    opacity: 1;
+    z-index: 3;
+    transform: translateY(0);
+  }
+`;
+
+export const Container = styled.div`
+  position: relative;
+`;
+
 export const BookmarkContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  border: solid black 0.1rem;
   border-radius: 0rem;
-  border: solid ${theme.colors.extra} 0.2rem;
-  background-color: ${theme.colors.primary};
-  color: white;
-  border-radius: 1rem;
-  width: calc(5rem + 10vw);
-  height: 4rem;
+  width: calc(4rem + 8vw);
+  height: 3rem;
   cursor: pointer;
-  transition: background-color 1s, color 1s, box-shadow 1s, border-radius 1s; /* Add transitions here */
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: -100%;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    transition: top 0.3s;
+  }
+
+  &:hover::before {
+    top: 0;
+  }
 
   &:hover {
-    border: solid ${theme.colors.extra} 0.2rem;
     background-color: white;
-    color: white;
-    border-radius: 1rem;
+    color: white; /* Text color on hover */
     box-shadow: 0rem 0.1rem 0.1rem grey;
-    transition: background-color 1.5s, color 1s, box-shadow 1s, border-radius 1s; /* Add transitions here */
   }
 
   &:not(:hover) {
-    transition: background-color 1s, color 1s, box-shadow 1s, border-radius 1s; /* Revert transition properties */
+    transition: background-color 1s, color 1s, box-shadow 1s, border-radius 1s;
   }
 
   &:active {
@@ -38,11 +69,53 @@ export const BookmarkContainer = styled.div`
 `;
 
 export const BookMarkTitle = styled.h3`
-  font-size: calc(0.8rem + 1vw);
-  transition: color 1s; /* Add a transition for color change */
-  color: white;
+  font-size: calc(0.5rem + 0.8vw);
+  color: black;
+  transition: color 0.3s;
 
   ${BookmarkContainer}:hover & {
-    color: black; /* Change color on hover */
+    z-index: 3;
+    color: white;
+  }
+`;
+
+export const DropDownContainer = styled.div`
+  box-sizing: border-box;
+  top: 3rem;
+  width: 100%;
+  background-color: black;
+  position: absolute;
+  z-index: 3;
+  font-size: calc(0.5rem + 0.8vw);
+  color: white;
+
+  border-top: none;
+  cursor: pointer;
+
+  padding-top: 0.3rem;
+  padding-bottom: 0.3rem;
+  transition: background-color 0.3s; /* Add transition for background-color */
+  &:hover {
+    color: ${theme.colors.secondary};
+  }
+  animation: ${fadeIn} 0.7s 0.1s forwards; /* Apply the animation with a 0.3s delay */
+  opacity: 0; /* Start with opacity set to 0 */
+
+  ${BookmarkContainer}:hover & {
+    z-index: 3;
+    color: white;
+    background-color: white; /* Change background color on hover */
+  }
+`;
+export const Add = styled.button`
+  font-size: calc(0.3rem + 0.5vw);
+  background-color: black;
+  color: white;
+  border: none;
+  cursor: pointer;
+
+  width: 100%;
+  &:hover {
+    color: ${theme.colors.primary};
   }
 `;
