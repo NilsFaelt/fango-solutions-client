e5c4f3e377e80522b35fc55a93dd2dd65ab7a032
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-10-13 16:46:35 +0200

    fixed cookie error

--- components/CookieBanner/CookieBanner.component.tsx
+++ components/CookieBanner/CookieBanner.component.tsx
@@ -1,46 +1,48 @@
-"use client";
-import React, { FC, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Button, Container, Title } from "./CookieBanner.styles";
 import { MainText } from "@/ui/display/MainText/MainText.component";
-import { StyledLinkBlack } from "@/styles";
+import { StyledLink, StyledLinkBlack } from "@/styles";
 
 export const CookieBanner: FC = () => {
-  const [renreder, setReRender] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const checkCookieConsent = () => {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (!cookie.startsWith("cookie_consent=")) {
+          setIsVisible(true);
+          return;
+        }
+      }
+    };
+
+    checkCookieConsent();
+  }, []);
 
   const handleClick = () => {
     const tenSecondsFromNow = new Date();
     tenSecondsFromNow.setTime(tenSecondsFromNow.getTime() + 10000);
-    const oneWeekFromNow = new Date();
-    oneWeekFromNow.setDate(oneWeekFromNow.getDate() + 7);
-
-    const expires = oneWeekFromNow.toUTCString();
+    const expires = tenSecondsFromNow.toUTCString();
     document.cookie = `cookie_consent=accepted; expires=${expires}; path=/`;
-    setReRender(!renreder);
-  };
-  function checkCookieConsent() {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      if (cookie.startsWith("cookie_consent=")) {
-        return true;
-      }
-    }
-    return false;
-  }
-  useEffect(() => {}, [renreder]);
 
-  if (!checkCookieConsent())
-    return (
+    setIsVisible(false);
+  };
+
+  return (
+    isVisible && (
       <Container>
         <Title>We Use Cookies</Title>
         <MainText color='black'>
-          FangoSolutions uses cookies to maintain a good user experience, Please
-          read our cookie policy before use
+          FangoSolutions uses cookies to maintain a good user experience. Please
+          read our cookie policy.
         </MainText>
-        <StyledLinkBlack color='black' href={"/cookie-policy"}>
+        <StyledLinkBlack href={"/cookie-policy"}>
           Read Cookie Policy
         </StyledLinkBlack>
         <Button onClick={handleClick}>ACCEPT</Button>
       </Container>
-    );
+    )
+  );
 };
