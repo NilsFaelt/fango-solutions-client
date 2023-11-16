b022b83f8a04d3cf7ba15f83b40612565c589d71
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-11-16 14:23:51 +0100

    test

--- components/UpdateBookmark/UpdateUrlsPopUp/UpdateUrlsPopUp.style.tsx
+++ components/UpdateBookmark/UpdateUrlsPopUp/UpdateUrlsPopUp.style.tsx
@@ -1,3 +1,4 @@
+import { mediaQueries, theme } from "@/styles";
 import styled from "styled-components";
 
 export const Container = styled.div`
@@ -39,9 +40,41 @@ export const InputLabelWrapper = styled.div`
   justify-content: flex-start;
   margin-top: 1rem;
   width: 100%;
+  @media (max-width: ${mediaQueries.mobile}) {
+    width: 10rem;
+    padding-left: 1rem;
+  }
 `;
 export const ButtonWrapper = styled.div`
   margin-top: 3rem;
   width: 16rem;
   align-self: center;
+  @media (max-width: ${mediaQueries.mobile}) {
+    width: 11rem;
+    padding-left: 1rem;
+  }
+`;
+export const PrimaryInput = styled.input<{ $width?: string }>`
+  width: 100%;
+  height: 2rem;
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: 0.05rem solid black;
+  border-bottom: 0.05rem solid ${theme.colors.primary};
+  color: white;
+  background-color: black;
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  &::placeholder {
+    color: white;
+  }
+  &:focus {
+    outline: none;
+  }
+  &[type="number"] {
+    -moz-appearance: textfield;
+  }
 `;
