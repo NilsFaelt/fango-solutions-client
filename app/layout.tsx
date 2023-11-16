a1cd43b91ae4cbf2112a2f4570358c4175028a77
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2023-11-16 14:10:40 +0100

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <meta name='viewport' content='width=device-width, user-scalable=no' />
       <body className={kanit.className}>
         <StyledComponentsRegistry>
           <ProviderWrapper>{children}</ProviderWrapper>
